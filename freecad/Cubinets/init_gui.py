36cc4523464b648d1c9763305914f9fc0002e1db
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-03-17 00:06:55 -0400

    Separate workbench

--- freecad/Cubinets/init_gui.py
+++ freecad/Cubinets/init_gui.py
@@ -3,45 +3,13 @@
 
 from FreeCAD import Gui
 
+from .Workbench import CubinetsWorkbench
 from .Commands import cmdNewParams , cmdAssemble , cmdSettings , cmdCutList
-from .Misc import asIcon
 
 
 Gui.addCommand('Cubinets_Assemble',cmdAssemble())
 Gui.addCommand('Cubinets_Settings',cmdSettings())
 Gui.addCommand('Cubinets_CutList',cmdCutList())
 Gui.addCommand('Cubinets_Sheet',cmdNewParams())
-
-
-class CubinetsWorkbench ( Gui.Workbench ):
-
-    MenuText = 'Cubinets'
-    ToolTip = 'Visualise cabinet assemblies using parametric templates.'
-    Icon = asIcon('Addon')
-
-    def Initialize ( self ):
-        
-        list = [
-            'Cubinets_Settings' , 
-            'Cubinets_Assemble' , 
-            'Cubinets_CutList' ,
-            'Cubinets_Sheet'
-        ]
-        
-        self.appendToolbar('Cubinets',list)
-        self.appendMenu('Cubinets',list)
-
-
-    def Activated ( self ):
-        pass
-
-
-    def Deactivated ( self ):
-        pass
-
-
-    def GetClassName ( self ):
-        return 'Gui::PythonWorkbench'
-
 
 Gui.addWorkbench(CubinetsWorkbench())
